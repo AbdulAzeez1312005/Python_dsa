e16f7face7ca3eb9156689e1557e6acfbc5a2644
Abdul Azeez <151101928+AbdulAzeez1312005@users.noreply.github.com>
2026-09-24 17:43:41 +0530

    Update Newque.py

--- Newque.py
+++ Newque.py
@@ -1,1 +1,17 @@
-print("hee")
+def rotate(nums, k):
+    for i in range(k):
+        last = nums[-1]
+
+        for j in range(len(nums) - 1, 0, -1):
+            nums[j] = nums[j - 1]
+
+        nums[0] = last
+
+
+if __name__ == "__main__":
+    nums = list(map(int, input().split()))
+    k = int(input())
+
+    rotate(nums, k)
+
+    print(nums)
